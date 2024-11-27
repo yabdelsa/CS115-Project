cd618c5d3dddffc607482bab7c6711507dee3de7
Zack Wilkinson <44929504+zackjwilk@users.noreply.github.com>
2024-11-27 01:12:22 -0500

    Update musicrecplus.py

--- musicrecplus.py
+++ musicrecplus.py
@@ -1,38 +1,38 @@
-menuStr = """Enter a letter to choose an option:\n
-            e - Enter preferences\n
-            r - Get recommendations\n
-            p - Show most popular artist\n
-            h - How popular is the most likes\n
-            m - Which user has the most likes\n
-            q - Save and quit\n"""
+menuStr = "Enter a letter to choose an option:\ne - Enter preferences\nr - Get recommendations\np - Show most popular artist\nh - How popular is the most likes\nm - Which user has the most likes\nq - Save and quit\n"
 
 database = []
 
-def newUser(file):
+def newUser(name, file):
+    """
+    Returns True if user's name is not found in file
+    Returns False if user's name is found in file
+    """
     names=[]
     for line in file:
         names.append(line.split(":")[0])
-    return newUser not in names
+    return name not in names
 
 def menu():
     option = input(menuStr).lower()
     while option not in ["erphmq"]:
         option = input(menuStr).lower()
 
-def enterPreferences(name):
+def enterPreferences(name, file):
+    artists = ""
     artist = input("Enter an artist that you like ( Enter to finish ): ")
     while artist:
-        if newUser(name):
-            
-try:
-    with open("musicrecplus.txt", "r") as file:
-        if newUser(file):
-            name = input("""Enter your name ( put a $ symbol
-                            after your name if you wish your preferences 
-                            to remain private ): """)
-            private = (name[-1]=="$")
-            enterPreferences(name)
-        menu()
-except:
-    with open("musicrecplus.txt", "w") as file:
-        menu()
+        artists += artist + ","
+        artist = input("Enter an artist that you like ( Enter to finish ): ")
+    artists = artists[:-1]
+    if newUser(name, open(file, "r")):
+        open(file, "w").write(name+":"+artists)
+
+def main(file):
+    name = input("Enter your name ( put a $ symbol after your name if you wish your preferences to remain private ): ")
+    private = (name[-1]=="$")
+    txt = open("musicrecplus.txt", "w") # create file if doesn't exist
+    if newUser(name, open(file, "r")):
+        enterPreferences(name, file)
+    menu() 
+
+main("musicrecplus.txt")
