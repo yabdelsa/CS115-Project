2f3e80e58aa447601f78a1703a28be6f3b35088f
Zack Wilkinson <44929504+zackjwilk@users.noreply.github.com>
2024-12-01 18:21:17 -0500

    Update musicrecplus.py

--- musicrecplus.py
+++ musicrecplus.py
@@ -1,115 +1,129 @@
-menuStr = """Enter a letter to choose an option:\n
-e - Enter preferences\n
-r - Get recommendations\n
-p - Show most popular artist\n
-h - How popular is the most popular\n
-m - Which user has the most likes\n
-q - Save and quit\n
+menuStr = """Enter a letter to choose an option:
+e - Enter preferences
+r - Get recommendations
+p - Show most popular artist
+h - How popular is the most popular
+m - Which user has the most likes
+q - Save and quit
 """
 
 database = []
 
-def newUser(name, file):
+def isNewUser(user, database):
     """
-    Returns True if user's name is not found in file
-    Returns False if user's name is found in file
+    Returns True if user's name is not found in file.
+    Returns False if user's name is found in file.
     """
-    names=[]
-    for line in file:
-        names.append(line.split(":")[0])
-    return name not in names
+    users = []
+    for line in database:
+        users.append(line.split(":")[0])
+    return user not in users
+
+def isPrivate(user):
+    return user.endswith("$")
 
 def menu():
     option = input(menuStr).lower()
-    while option not in ["e", "r", "p", "h", "m", "q"]:
+    while option not in ["e","r","p","h","m","q"]:
         option = input(menuStr).lower()
     return option
 
-def enterPreferences(name, file):
+def enterPreferences(user, database):
     """
-    Prompts the user to enter their favorite artists and updates the database. 
+    Prompts the user to enter their favorite artists and
+    updates the database. 
     """
-    artists = ""
-    artist = input("Enter an artist that you like ( Enter to finish ): ").title()
+    artists = []
+    artist = input("Enter an artist that you like ( Enter to finish ): ").title().strip()
     while artist:
-        artists += artist + ","
-        artist = input("Enter an artist that you like ( Enter to finish ): ").title()
-    artists = artists[:-1]
-    artists = artists.rstrip(',')
-    artists_list = artists.split(',')
-    artists_list.sort()
-    artists_s = ', '.join(artists_list)
-    if newUser(name, open(file, "r")):
-        open(file, "w").write(name+":"+artists_s)
-        
+        artists.append(artist)
+        artist = input("Enter an artist that you like ( Enter to finish ): ").title().strip()
+    artists.sort()
+    artistsStr = ",".join(artists)
+    for i in range(len(database)):
+        user2 = database[i].split(":")[0]
+        if user == user2:
+            database[i] = user + ":" + artistsStr
+            return None
+    database.append(user + ":" + artistsStr)
 
-def getRecommendations(name, file): 
+def getRecommendations(user, database): 
+    pass
 
-
-def showMostPopularArtist(file):
-    artist_counts = {} 
-    lines = open(file, "r").readlines()
-    for line in lines:
-        user, artists = line.split(":")
-        
-        if not user.endswith("$"):
-            for artist in artists.strip().split(','):
-                artist_counts[artist] = artist_counts.get(artist, 0) + 1
-    if artist_counts:
-        most_popular_count = max(artist_counts.values()) 
-        most_popular_count = [artist for artist, count in artist_counts.items() if count == most_popular_count] 
-        print("Most Popular Artist(s): " + ', '.join(most_popular_count))
-    else: 
-        print("Sorry. no artists found") 
-        
-
-def MostPopularCount(file):
+def showMostPopularArtist(database):
     """
-    Shows how popular the most liked artist is by counting the number of users who like it
+    Finds top three artists who appear most in database
+    and prints each of their names.
     """
-    artist_counts = {}
-    lines = open(file, "r").readlines()
-    for line in lines:
+    artistCounts = {}
+    for line in database:
         user, artists = line.split(":")
-        if not user.endswith("$"):
-            for artist in artists.strip().split(','):
-                artist_counts[artist] = artist_counts.get(artist, 0) + 1
-    if artist_counts: 
-        most_liked_artist = max(artist_counts, key = artist_counts.get)
-        print(most_liked_artist)
+        if not isPrivate(user):
+            for artist in artists.split(','):
+                artistCounts[artist] = artistCounts.get(artist, 0) + 1
+    if artistCounts:
+        mostPopularArtists = []
+        for i in range(3):
+            for artist in artistCounts:
+                if artistCounts[artist] == max(artistCounts.values()):
+                    mostPopularArtists.append(artist)
+                    del artistCounts[artist]
+                    break
+        for artist in mostPopularArtists:
+            print(artist)
     else: 
-        print("Sorry, no artists found")
+        print("Sorry, no artists found.")     
 
+def mostPopularCount(database):
+    """
+    Finds the artist who appears most in database and
+    prints number of times it is found.
+    """
+    artistCounts = {}
+    for line in database:
+        user, artists = line.split(":")
+        if not isPrivate(user):
+            for artist in artists.split(','):
+                artistCounts[artist] = artistCounts.get(artist, 0) + 1
+    if artistCounts:
+        mostPopularCount = max(artistCounts.values())
+        print(mostPopularCount)
+    else: 
+        print("Sorry, no artists found.")
 
-def showUserWithMostLikes(file):
+def showUserWithMostLikes(database):
     """
     Displays the user(s) with the most liked artists.
     """
-    max_likes = 0
-    most_liked_users = []
+    pass
 
-
-def main(file):
-    name = input("Enter your name ( put a $ symbol after your name if you wish your preferences to remain private ): ")
-    private = (name[-1]=="$")
-    txt = open("musicrecplus.txt", "w") # create file if doesn't exist
-    if newUser(name, open(file, "r")):
-        print("Welcome, new user!")
-        enterPreferences(name, file)
-        
+def main(fileName, database):
+    user = input("Enter your name ( put a $ symbol after your name if you wish your preferences to remain private ): ")
+    
+    try:
+        file = open(fileName, "r")
+        database = open(fileName, "r").readlines()
+    except:
+        file = open(fileName, "w")
+    
+    if isNewUser(user, database):
+        enterPreferences(user, database)
+    
     option = menu() 
     while option != 'q': 
         if option == 'e':
-            enterPreferences(name, file)
+            enterPreferences(user, database)
         elif option == 'r':
             print("Get Recommendations") 
         elif option == 'p':
-            print("Show Most Popular Artist")
+            showMostPopularArtist(database)
         elif option == 'h':
-            print("How Popular is the Most Popular")
+            mostPopularCount(database)
         elif option == 'm':
             print("Which User Has the Most Likes")
         option = menu()
-    print("Save and Quit")
+    with open(fileName, "w") as file:
+        for line in database:
+            file.write(line)
             
-main("musicrecplus.txt")
+main("musicrecplus.txt", database)
